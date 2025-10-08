ed408308be785d04e70eda087dd8536cf74b9baf
Audrey Shin <audreyshin23@gmail.com>
2025-10-08 16:17:44 -0400

    fixed

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: join(__dirname, '.env') });
+// Load .env from project root, not server directory
+dotenv.config({ path: join(__dirname, '..', '.env') });
 
 import express from "express";
 import mongoose from "mongoose";
